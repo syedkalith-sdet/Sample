2bd01ee816856af58985ea0cf8c603057e092983
Admin <Admin@DESKTOP-TAR5APO>
2026-07-18 09:16:38 +0530

    This is ATE Branch Commit

---
+++ src/test/java/demo/Demo1.java
@@ -0,0 +1,9 @@
+package demo;
+
+public class Demo1 {
+	public static void main(String[] args) {
+		System.out.println("Hello GitHub");
+		System.out.println("New class");
+		System.out.println("This is ATE Branch");
+	}
+}
